755e68bfb107d14e9fe928c0230823c5b2ab193f
shadyzoz <shadyzoz@163.com>
2020-09-30 01:30:10 +0800

    fix test

--- test/pages/api/hello.test.ts
+++ test/pages/api/hello.test.ts
@@ -2,6 +2,6 @@ import * as assert from 'assert';
 
 describe('test/pages/api/index.test.ts', function() {
   it('test', () => {
-    assert({ a: 1 }.a === { a: 2 }.a);
+    assert({ a: 1 }.a === { a: 2, b: 1 }.b);
   });
 });
